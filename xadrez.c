9d61353671fcf8f72e312e26a200f00c58fc9663
fabiogbaptista <fabiogbaptista@gmail.com>
2025-11-01 00:12:02 +0000

    usando recursividade

--- xadrez.c
+++ xadrez.c
@@ -1,34 +1,50 @@
 #include <stdio.h>
+
+void movimentacao_torre(int casas) {
+    if (casas > 0) {
+        printf("Direita.\n");
+        movimentacao_torre(casas - 1);
+    }
+}
+
+void movimentacao_bispo(int casas) {
+    // Loop externo: movimentação vertical
+    for (int i = 0; i < casas; i++) {
+        // Loop interno: movimentação horizontal (1 passo por iteração vertical)
+        for (int j = 0; j < 1; j++) {
+            printf("Cima, direita.\n");
+        }
+    }
+}
+
+void movimentacao_rainha(int casas) {
+    if (casas > 0) {
+        printf("Esquerda.\n");
+        movimentacao_rainha(casas - 1);
+    }
+}
+
+
 int main(){
 
-    // Declarando as variáveis
-    int movtorre =5, movbispo = 5, movrainha = 8;
-    int moviementocompleto = 1;
+   int movimentocompleto = 1;
 
     // Lógica do movimento da torre
     printf("Movendo a torre:\n");
-    while (movtorre >0){
-        printf("Direita.\n");
-        movtorre--;
-    }
+    movimentacao_torre(5);
 
     // Lógica do movimento do bispo
     printf("Movendo o bispo:\n");
-    do {
-        printf("Cima, direita.\n");
-        movbispo--;
-    } while (movbispo >0);
+    movimentacao_bispo(5);
 
     // Lógica do movimento da rainha
     printf("Movendo a rainha:\n");
-    for (int i = 0; i < movrainha; i++) {
-        printf("Esquerda.\n");
-    }
+    movimentacao_rainha(8);
 
     //Lógica do movimento do cavalo
     printf("Movendo o cavalo:\n");
 
-    while(moviementocompleto--){
+    while(movimentocompleto--){
 
         for (int i=0; i < 2; i++)
         {
